e0ff21563d91d143631ce1d7e8552ab4eb1c553c
Jon <59704644+JonNData@users.noreply.github.com>
2020-07-13 16:07:21 -0700

    Add files via upload

---
+++ fundamentals/stack.py
@@ -0,0 +1,65 @@
+"""
+A stack is a data structure whose primary purpose is to store and
+return elements in Last In First Out order. 
+
+1. Implement the Stack class using an array as the underlying storage structure.
+   Make sure the Stack tests pass.
+2. Re-implement the Stack class, this time using the linked list implementation
+   as the underlying storage structure.
+   Make sure the Stack tests pass.
+3. What is the difference between using an array vs. a linked list when 
+   implementing a Stack?
+"""
+from linked_list import Node, LinkedList
+# Arrays
+# class Stack:
+#     def __init__(self, ls=[]):
+#         self.size = 0
+#         self.storage = ls
+
+#     def __len__(self):
+#         if self.size <= 0:
+#             return 0
+#         else:
+#             return self.size
+
+#     def push(self, value):
+#         self.size +=1
+#         return self.storage.append(value)
+
+#     def pop(self):
+#         if self.size == 0:
+#             return None
+#         else:
+#             self.size += -1
+#             return self.storage.pop(-1)
+
+
+# Linked Lists
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.link = LinkedList()
+        self.top = None
+
+    def __len__(self):
+        if self.size <= 0:
+            return 0
+        else:
+            return self.size
+
+    def push(self, value):
+        self.size +=1
+        self.link.add_to_tail(value)
+        self.top = self.link.tail
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        else:
+            self.top = self.link.remove_tail()
+            self.size += -1
+            return self.top
+
+# When using an array you can reference a whole list that has an order
+# When using a linked list the Stack only cares about what is at the top
